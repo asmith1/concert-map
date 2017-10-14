7f0b6545f45c6d9b7b4ead1a573a4d3d8b9f57d1
Ashley Smith <Ashley@Ashleys-MacBook-Pro-6.local>
2017-10-14 03:22:47 -0400

    initial commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+Hey guys! This is me trying again from scratch. So I took a lot of my 20 code
+and just put it here. It runs using "python -m SimpleHTTPServer" on port 8000.
+It's completely independent of everything else, but I didn't want to create
+an entirely new repository.
---
+++ get_map.js
@@ -0,0 +1,60 @@
+    function initMap() 
+    {
+      infowindow = new google.maps.InfoWindow();
+        myLat = 42.39674;
+        myLng = -71.121815;
+        me = new google.maps.LatLng(myLat, myLng);
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: me
+        });
+        getGeolocation();
+    }
+
+    function getGeolocation() 
+    {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+          myLat = position.coords.latitude;
+          myLng = position.coords.longitude;
+          renderMap();
+          addCircle();
+        }); 
+      }
+      else {
+        alert("Geolocation not supported by your browser");
+      }
+    }
+
+    function renderMap() 
+    {
+      me = new google.maps.LatLng(myLat, myLng);
+      map.panTo(me);
+      var marker = new google.maps.Marker({
+        position: me
+      });
+      marker.setMap(map);
+      google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent("Closest T-Stop: " + findClosestStop());
+          infowindow.open(map, marker);
+      });
+    }
+
+    function makeMarker(options) {
+        var circleMark = new google.maps.Circle(options);
+        circleMark.setMap(map);
+    }
+
+    function addCircle()
+    {
+      makeMarker({
+            map: map,
+            fillColor: "#FF0000",
+            fillOpacity: 0.5,
+            strokeColor: "#FF0000",
+            strokeWeight: 1,
+            center: myLat,
+            position: myLng,
+            radius: 500
+        });
+    }
